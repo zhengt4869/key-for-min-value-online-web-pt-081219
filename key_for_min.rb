48b242780c547416fd4549655cd743aa3166d156
KG ZZ <czheng5h88@gmail.com>
2019-09-11 18:16:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  c_min = 100000
+  res = ""
+  name_hash.each { |key, number|
+    if number < c_min
+      c_min = number
+      res = key 
+    end 
+  }
 end
